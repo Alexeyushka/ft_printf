2500b5cc7ace8181c340003c287b9b48153cdcd8
Jazlyn Martyn rivers <jmartyn-@at-f6.21-school.ru>
2019-07-01 21:30:44 +0300

    v_2

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: jmartyn- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 22:09:38 by jmartyn-          #+#    #+#             */
-/*   Updated: 2019/07/01 21:21:02 by jmartyn-         ###   ########.fr       */
+/*   Updated: 2019/07/01 21:30:25 by jmartyn-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,62 +14,68 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int		parse_arg(const char *print)
+int        parse_arg(const char *print)
 {
-	int i;
-
-	i = 0;
-	while (print[i] != '\0')
-	{
-		if (print[i] == '%')
-		{
-			if (print[i + 1] == 's')
-			{
-				return (1);
-			}
-		}
-		i++;
-	}
-	return (0);
+    int i;
+    
+    i = 0;
+    while (print[i] != '\0')
+    {
+        if (print[i] == '%')
+        {
+            if (print[i + 1] == 's')
+            {
+                return (1);
+            }
+        }
+        i++;
+    }
+    return (0);
 }
 
-void	ft_printf(char *print, char *arg)
+void    ft_printf(const char *format, ...)
 {
-	
-	int i;
-	int j;
+    
+    va_list list;
+	va_start(list, format);
+    char *arg = va_arg(list, char*);
 
-	j = 0;
-	i = 0;
-	if (parse_arg(print) == 1)
-	{
-		while (print[i] != '\0')
-		{
-			if (print[i] == '%' && print[i + 1] == 's')
-			{
-				while (arg[j] != '\0')
-				{
-					write(1, &arg[j], 1);
-					j++;
-				}
-				i = i + 2;
-			}
-			write(1, &print[i], 1);
-			i++;
-		}
-	}
+    int i;
+    int j;
+    
+    j = 0;
+    i = 0;
+    if (parse_arg(format) == 1)
+    {
+        while (format[i] != '\0')
+        {
+            if (format[i] == '%' && format[i + 1] == 's')
+            {
+                while (arg[j] != '\0')
+                {
+                    write(1, &arg[j], 1);
+                    j++;
+                }
+                i = i + 2;
+            }
+            write(1, &format[i], 1);
+            i++;
+        }
+    }
+    va_end(list);
 }
 
 int main()
 {
-	char *name;
-	name = "John";
-
-	printf("=====================\n");
-	printf("Printf output:\n");
-	printf("Hello %s\n", name);
-	printf("=======\n");
-	printf("ft_printf output:\n");
-	ft_printf("Hello %s", name);
-	return (0);
+    const char *name;
+    name = "John";
+    
+    printf("=====================\n");
+    printf("Printf output:\n");
+    printf("Hello %s\n", name);
+    printf("=======\n");
+    printf("ft_printf output:\n");
+    ft_printf("Hello %s", name);
+    //    printf("\n======================\n");
+    return (0);
 }
