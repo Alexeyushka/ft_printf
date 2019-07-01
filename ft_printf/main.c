15d50afc99b5034433a438184f18e61b72c027f9
Jazlyn Martyn rivers <jmartyn-@at-f4.21-school.ru>
2019-07-01 23:20:22 +0300

    v_3

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,26 +6,62 @@
 /*   By: jmartyn- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 22:09:38 by jmartyn-          #+#    #+#             */
-/*   Updated: 2019/07/01 21:30:25 by jmartyn-         ###   ########.fr       */
+/*   Updated: 2019/07/01 22:31:05 by jmartyn-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+#include <stdlib.h> //atoi
+
+void    handle_d(const char *format, int arg)
+{
+
+    printf("%d", arg); //need to debug
+    
+}
+
+void     handle_s(const char *format, char *arg)
+{
+    int i;
+    int j;
+    
+    j = 0;
+    i = 0;
+    while (format[i] != '\0')
+    {
+        if (format[i] == '%' && format[i + 1] == 's')
+        {
+            while (arg[j] != '\0')
+            {
+                write(1, &arg[j], 1);
+                j++;
+            }
+            i = i + 2;
+        }
+        write(1, &format[i], 1);
+        i++;
+    }
+}
 
 int        parse_arg(const char *print)
 {
     int i;
     
     i = 0;
-    while (print[i] != '\0')
-    {
+	while (print[i] != '\0')
+	{
         if (print[i] == '%')
-        {
-            if (print[i + 1] == 's')
+		{
+        	if (print[i + 1] == 's')
             {
                 return (1);
+            }
+            if (print[i + 1] == 'd')
+            {
+                return (2);
             }
         }
         i++;
@@ -35,32 +71,19 @@ int        parse_arg(const char *print)
 
 void    ft_printf(const char *format, ...)
 {
-    
+    char *arg;
+    int number;
     va_list list;
 	va_start(list, format);
-    char *arg = va_arg(list, char*);
-
-    int i;
-    int j;
+	arg = va_arg(list, char*);
     
-    j = 0;
-    i = 0;
+    number = ((int)va_arg(list, char*)); //need to debug
+
     if (parse_arg(format) == 1)
+        handle_s(format, arg);
+    if (parse_arg(format) == 2)
     {
-        while (format[i] != '\0')
-        {
-            if (format[i] == '%' && format[i + 1] == 's')
-            {
-                while (arg[j] != '\0')
-                {
-                    write(1, &arg[j], 1);
-                    j++;
-                }
-                i = i + 2;
-            }
-            write(1, &format[i], 1);
-            i++;
-        }
+        handle_d(format, number);
     }
     va_end(list);
 }
@@ -68,14 +91,20 @@ void    ft_printf(const char *format, ...)
 int main()
 {
     const char *name;
-    name = "John";
+	int number;
+	number = 42;
+    //name = "John";
     
     printf("=====================\n");
     printf("Printf output:\n");
-    printf("Hello %s\n", name);
+//  char output
+//    printf("Hello %s\n", name);
+	printf("hello %d\n", number);
+
     printf("=======\n");
     printf("ft_printf output:\n");
-    ft_printf("Hello %s", name);
-    //    printf("\n======================\n");
+//  char output
+//    ft_printf("Hello %s\n", name);
+	ft_printf("Hello %d\n", number);
     return (0);
 }
