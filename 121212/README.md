754650e9153657b4c7619517217edbb80ce4dcdd
alexeyushka <homer147@yandex.ru>
2019-10-04 19:44:27 +0300

    deleting

--- 121212/README.md
+++
@@ -1,2 +0,0 @@
-# ft_printf
-42 School Project | Representation of printf function
--- for test/main.c
+++
@@ -1,9 +0,0 @@
-#include "includes/ft_printf.h"
-
-int	main()
-{
-	char *str = "Hello";
-	ft_printf("%s", "test");
-	return (0);
-}
-
